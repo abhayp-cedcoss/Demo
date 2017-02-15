91a9d97a8f9cca21b51903ea7bf196004e81a7e8
test <test@test.com>
2017-02-15 15:00:16 +0530

    third change

--- demo.php
+++ demo.php
@@ -8,7 +8,53 @@
 <link href="main1.css" rel="stylesheet">
 </head>
 <body>
- First
-<div id="main"> Abhay Pratap singh</div>
+<div class="container-fluid">
+ <section id="one">
+<header id="header1">
+  <h1>Ipsum lorem dolor aliquam ante commodo<br>
+magna sed accumsan arcu neque.</h1></header>
+<p >Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem <br>vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id <br>nisi accumsan pellentesque commodo <br> blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem s<br>emper nunc nisi lorem vulputate lorem neque cubilia.</p>
+<ul class="actions">
+<li>
+<a class="default" href="#" role="button">Learn More</a>
+</li>
+</ul>
+</section>
+  <h3>Recent Work</h3>
+  <div class="row">
+    <div class="col-sm-4" style="background-color:lavender;">
+          <a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img src="index.jpeg" class="img-rounded"/></a>
+          <h3>Magna sed consequat tempus</h3>
+          <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+    </div>
+    <div class="col-sm-4" style="background-color:lavenderblush;"><img src="images.jpeg" class="img-rounded"/>
+                 <h3>Ultricies lacinia interdum</h3>
+                <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-4" style="background-color:lavender;">
+          <a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img src="index.jpeg"class="img-rounded" /></a>
+          <h3>Magna sed consequat tempus</h3>
+          <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+    </div>
+    <div class="col-sm-4" style="background-color:lavenderblush;"><img src="images.jpeg" class="img-rounded"/>
+                 <h3>Ultricies lacinia interdum</h3>
+                <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+    </div>
+  </div>
+   <div class="row">
+    <div class="col-sm-4" style="background-color:lavender;">
+          <a class="image fit thumb" style="cursor: pointer; outline: 0px none;"><img src="index.jpeg" class="img-rounded"/></a>
+          <h3>Magna sed consequat tempus</h3>
+          <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+    </div>
+    <div class="col-sm-4" style="background-color:lavenderblush;"><img src="images.jpeg" class="img-rounded"/>
+                 <h3>Ultricies lacinia interdum</h3>
+                <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+    </div>
+  </div>
+  </div>
 </body>
 </html>
